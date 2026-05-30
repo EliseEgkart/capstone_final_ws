ca1dcbddc7fe91c83249ad8a304f93452136a280
Hyeongjin Kim <ky942400@gmail.com>
2026-05-31 03:59:47 +0900

    student update

--- src/all_in_one_package/launch/manipulator_all_in_one_v3_student.launch.py
+++ src/all_in_one_package/launch/manipulator_all_in_one_v3_student.launch.py
@@ -168,6 +168,7 @@ def generate_launch_description():
             'manipulator_task_cmd_topic': '/manipulator_task_cmd',
             'manipulator_task_result_topic': '/manipulator_task_result',
             'manipulator_task_state_topic': '/manipulator_task_state',
+            'final_approach_enable': False,
         }]
     )
 
